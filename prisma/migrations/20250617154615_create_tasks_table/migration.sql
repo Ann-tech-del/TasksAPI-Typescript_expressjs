940425be318c3fad06a7afef070591ca77c4277a
Ann-tech-del <annodhiambo87@gmail.com>
2025-06-17 18:47:48 +0300

    made changes on the Schema

--- prisma/migrations/20250617154615_create_tasks_table/migration.sql
+++ prisma/migrations/20250617154615_create_tasks_table/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE "tasks" (
+CREATE TABLE "tasksv2" (
     "ID" TEXT NOT NULL,
     "task_tittle" TEXT NOT NULL,
     "task_description" TEXT NOT NULL,
     "is_completed" BOOLEAN NOT NULL DEFAULT false,
     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "tasks_pkey" PRIMARY KEY ("ID")
+    CONSTRAINT "tasksv2_pkey" PRIMARY KEY ("ID")
 );
